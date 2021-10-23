2ce1cd0eec22b3a4d183081889f24e10913888fa
BuildTools <santiago.moreno.2009@gmail.com>
2021-10-22 21:03:25 -0300

    feat: added User model

---
+++ src/sequelize/models/user.ts
@@ -0,0 +1,33 @@
+import { DataTypes, Model } from 'sequelize';
+
+export default (sequelize) => {
+    //Extend Model class
+    class User extends Model {}
+
+    //Initialize User
+    User.init({
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },{
+        sequelize,
+        modelName: 'User'
+    })
+}
+
+
+
