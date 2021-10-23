53d1aba96cb14e72d64dd202675c9f85d85d6904
BuildTools <santiago.moreno.2009@gmail.com>
2021-10-22 21:07:57 -0300

    refactor: moved sequelize connection check and sync to server.ts

--- src/sequelize/index.ts
+++ src/sequelize/index.ts
@@ -3,25 +3,4 @@ import { Sequelize } from "sequelize";
 const DB_URL = process.env.DB_URL;
 
 //Setting up db connection
-const sequelize = new Sequelize(`${DB_URL}`);
-
-//Check connection
-const checkConnection = async() => {
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-
-      } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
-} 
-checkConnection();
-
-//Syncronize all models
-const syncSequelize = async() => {
-    await sequelize.sync({ force: true });
-}
-syncSequelize();
-
-
-export default sequelize;
+export const sequelize = new Sequelize(`${DB_URL}`);
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 
+import { sequelize } from './sequelize/index';
+
 const app: Application = express();
 const port = process.env.port;
 
@@ -10,3 +12,21 @@ app.get('/', (req: Request, res: Response): void => {
 
 app.use(cors);
 app.listen(port, (): void => console.log(`Server running on port ${port}`))
+
+//Check sequelize connection
+const checkConnection = async() => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+} 
+checkConnection();
+
+//Syncronize all models
+const syncSequelize = async() => {
+    await sequelize.sync({ force: true });
+}
+syncSequelize();
