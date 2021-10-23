8f2d0922f73a10b31ec7a32f61b41c8bd11c6643
BuildTools <santiago.moreno.2009@gmail.com>
2021-10-23 18:20:25 -0300

    feat: added association-related logic to sequelize db index

--- src/sequelize/index.ts
+++ src/sequelize/index.ts
@@ -1,15 +1,26 @@
 import { Sequelize } from "sequelize";
 import User from './models/user'
 
-
+//Env var
 const DB_URL = process.env.DB_URL;
 
 //Setting up db connection
 export const sequelize = new Sequelize(`${DB_URL}`);
 
+//Db types
+type Database = {[key: string]: any}
+type KeyOfDB = Extract<keyof Database, string>
+
 //Create Models
-const db = {
+const db: Database = {
     User: User(sequelize)
 }
+
+//Associations
+Object.keys(db).forEach((m: KeyOfDB) => {
+    if(db[m].associate) {
+        db[m].associate(db);
+    }
+})
 
 export default db;
