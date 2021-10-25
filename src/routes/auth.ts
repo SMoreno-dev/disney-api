d5874af11b4b919ebbb3fe1f2fb202c756b36b53
BuildTools <santiago.moreno.2009@gmail.com>
2021-10-24 21:50:47 -0300

    feat: setting up auth routes

---
+++ src/routes/auth.ts
@@ -0,0 +1,10 @@
+import express from "express";
+const router = express.Router()
+
+//Register
+router.post('/register');
+
+//Login
+router.post('/login');
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -6,12 +6,16 @@ import { sequelize } from './sequelize/index';
 const app: Application = express();
 const port = process.env.port;
 
-app.get('/', (req: Request, res: Response): void => {
-    res.send('<h1>Server Running</h1>');
-})
-
 app.use(cors);
 app.listen(port, (): void => console.log(`Server running on port ${port}`));
+
+//Routes
+app.get('/', (req: Request, res: Response): void => {
+    res.send('<h1>Server Running</h1>');
+});
+
+//Auth
+app.use('/auth', require('./routes/auth'));
 
 //Check sequelize connection
 (async() => {
