f04961431a675fb14e16b64c8130072866f20e42
BuildTools <santiago.moreno.2009@gmail.com>
2021-10-26 19:13:36 -0300

    feat: added movie preload data

---
+++ src/sequelize/preload/movie.ts
@@ -0,0 +1,26 @@
+const movie = [
+    {
+        img: "https://m.media-amazon.com/images/M/MV5BNTI4ODQ1MzAzNV5BMl5BanBnXkFtZTgwMzU4NzMxMDE@._V1_.jpg",
+        title: "Aladdin",
+        created: "1992-11-25",
+        rating: 4
+    },
+    {
+        img: "https://static.wikia.nocookie.net/english-voice-over/images/5/54/The_Lion_King_1994_DVD_Cover.PNG/revision/latest?cb=20160108215359&path-prefix=sv",
+        title: "The Lion King",
+        created: "1994-07-07",
+        rating: 5
+    },
+    {
+        img: "https://static.wikia.nocookie.net/disney/images/5/56/Snow-White-and-the-Seven-Dwarfs-Poster.jpg/revision/latest/scale-to-width-down/250?cb=20160923160034&path-prefix=es",
+        title: "Snow White and the Seven dwarfs",
+        created: "1938-01-26",
+        rating: 5
+    },
+    {
+        img: "https://m.media-amazon.com/images/M/MV5BZTAzNWZlNmUtZDEzYi00ZjA5LWIwYjEtZGM1NWE1MjE4YWRhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg",
+        title: "Finding Nemo",
+        created: "2003-07-03",
+        rating: 5
+    }
+]
