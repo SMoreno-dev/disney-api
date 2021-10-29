f031070fdbae59ba134b50f0c4c5de231d7806cf
BuildTools <santiago.moreno.2009@gmail.com>
2021-10-29 17:44:37 -0300

    feat: Adding AuthUtil class with jwt methods

---
+++ src/controllers/utils/auth.ts
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+const secret: any = process.env.SECRET;
+
+export default class AuthUtil {
+  static signToken(userUUID: string) {
+    //Sign user uuid
+    return jwt.sign({ userUUID }, secret, (err: any, token: any) => token);
+  }
+
+  static verifyToken(bearerHeader: string) {
+    //Split bearer header
+    const bearer = bearerHeader.split(" ");
+    //Select token
+    const bearerToken = bearer[1];
+    //Verify token
+    return jwt.verify(
+      bearerToken,
+      secret,
+      (err: any, authData: any) => authData
+    );
+  }
+}
