a5a063184935c714687a5cf01e0309f66641840e
BuildTools <santiago.moreno.2009@gmail.com>
2021-10-31 15:43:57 -0300

    style: removed comment from character controller `list` method

--- src/controllers/character.ts
+++ src/controllers/character.ts
@@ -83,13 +83,6 @@ export default class Character {
       //Look for character matching queries
       const characters = await db.Character.findAll({
         where: CharacterUtil.buildListWhereObject(req.query),
-        // include: [
-        //   {
-        //     model: db.Movie,
-        //     where: CharacterUtil.buildIncludeWhereObject(req.query),
-        //     required: false,
-        //   },
-        // ],
       });
 
       //If no characters are found...
